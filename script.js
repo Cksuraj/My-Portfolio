876ff403744dbede882d7a34d1c4b8705211ef73
Cksuraj <sjchaudhary00@gmail.com>
2024-01-01 15:51:42 +0530

    Web3Form, Resume, layout changes added

--- script.js
+++ script.js
@@ -1,67 +1,86 @@
+// Using Web3Forms
 
+const form = document.getElementById('form');
+const result = document.getElementById('result');
 
-// Google sheet (This code store the tha data in google sheet )
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzSUXndJ4Hfam9A55_P5y9Cjjw9xHXa9HD-ppqld9__ylz1ku3QPYiC2OTmOEtUARdm/exec'
-const form = document.forms['submit-to-google-Sheet']
-const msg = document.getElementById("msg")
+form.addEventListener('submit', function(e) {
+    const formData = new FormData(form);
+    e.preventDefault();
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-            msg.innerHTML = "Your response has been submited !"
-            setTimeout(function () {
-                msg.innerHTML = ""
-            }, 5000)
-            form.reset()
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
         })
-        .catch(error => console.error('Error!', error.message))
-})
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 2000);
+        });
+});
 
+// Funtion fro displaying the skill and education
 
-// Funtion fro displaying the skill and education 
-
-var tablinks = document.getElementsByClassName("tab-links")
-var tabcontents = document.getElementsByClassName("tab-contents")
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname) {
-    for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-    }
-    for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab")
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
 }
 
-// Humburge Menue 
+// Humburge Menue
 
 let sidemenu = document.getElementById("sidemenu");
 
 function openmenu() {
-    sidemenu.style.right = "0"
+  sidemenu.style.right = "0";
 }
 
 function closemenu() {
-    sidemenu.style.right = "-200px";
+  sidemenu.style.right = "-200px";
 }
-
-
 
 // Funtion for displaying more project
 
-
-let loadMoreBtn = document.querySelector('.btn');
+let loadMoreBtn = document.querySelector(".seemore-btn");
 let currentItem = 2;
 
 loadMoreBtn.onclick = () => {
-let boxes = [...document.querySelectorAll('.work')];
-for (var i = currentItem; i < currentItem + 2; i++) {
-boxes[i].style.display = 'inline-block';
-}
-currentItem += 2;
+  let boxes = [...document.querySelectorAll(".work")];
+  for (var i = currentItem; i < currentItem + 2; i++) {
+    boxes[i].style.display = "inline-block";
+  }
+  currentItem += 2;
 
-if (currentItem >= boxes.length) {
-loadMoreBtn.style.display = 'none'
-}
-}
+  if (currentItem >= boxes.length) {
+    loadMoreBtn.style.display = "none";
+  }
+};
